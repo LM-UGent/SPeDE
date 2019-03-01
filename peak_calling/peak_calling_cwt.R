9e9ba9105c0c4b4659b3c4262e7211c3c61729fb
SPeDE <46600490+LM-UGent-SPeDE@users.noreply.github.com>
2019-03-01 11:18:56 +0100

    Update peak_calling_cwt.R

--- peak_calling/peak_calling_cwt.R
+++ peak_calling/peak_calling_cwt.R
@@ -5,18 +5,17 @@ folder <- args[1]
 dir.create("PKL4DICE")
 
 for (infile in list.files(folder)){
-    print(infile)
     path = sprintf("%s/%s",folder,infile)
     path_out <- sprintf("PKL4DICE/PKL_%s",infile)
-
-    #Loading example data.
+    d <- data.frame(V1=0,V2=0)
+    #Loading data.
     MS_data <- read.csv(path, sep="",header=FALSE)
     #converting m/z values to a vector
     MS.test <- as.vector(MS_data[,2])
 
     SNR.Th <- 3 #setting the signal to noise ratio threshold
-    amp.Th <- 0.0001 #setting the minimum required relative amplitude threshold of peaks
-    try({peakInfo <- peakDetectionCWT(MS.test,SNR.Th=SNR.Th,ampTh=ampTh)
+    ampTh <- 0.0001 #setting the minimum required relative amplitude threshold of peaks
+    try({peakInfo <- peakDetectionCWT(MS.test,SNR.Th=SNR.Th,amp.Th=ampTh)
     majorPeakInfo <- peakInfo$majorPeakInfo
     betterPeakInfo <- tuneInPeakInfo(MS.test,majorPeakInfo)
     peakIndex <- betterPeakInfo$peakIndex
@@ -26,4 +25,5 @@ for (infile in list.files(folder)){
     d$SNR <- betterPeakInfo$peakSNR
 
     #writing peak list in csv format
-    write.table(d, file=path_out,append = FALSE,sep="\t", quote=FALSE,row.names=FALSE,col.names=c("Centroid Mass","S/N"))
+    write.table(d, file=path_out,append = FALSE,sep="\t", quote=FALSE,row.names=FALSE,col.names=c("Centroid Mass","S/N"))})
+    }
