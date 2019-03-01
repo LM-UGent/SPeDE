f75972f71799352419ed618459f50cbf674afb66
SPeDE <46600490+LM-UGent-SPeDE@users.noreply.github.com>
2019-03-01 10:57:21 +0100

    Update peak_calling_cwt.R

--- peak_calling/peak_calling_cwt.R
+++ peak_calling/peak_calling_cwt.R
@@ -1,20 +1,13 @@
 library(MassSpecWavelet)
 args <- commandArgs(TRUE)
 
-#folder <- args[1]
-folder <- "FullMS"
-
+folder <- args[1]
 dir.create("PKL4DICE")
-dir.create("PKL4PPMC_TRIMMED")
-dir.create("PKL4PPMC")
-
 
 for (infile in list.files(folder)){
     print(infile)
     path = sprintf("%s/%s",folder,infile)
     path_out <- sprintf("PKL4DICE/PKL_%s",infile)
-    path_trim <- sprintf("PKL4PPMC_TRIMMED/PKLint_%s",infile)
-    path_ppmc <- sprintf("PKL4PPMC/PKL_%s",infile)
 
     #Loading example data.
     MS_data <- read.csv(path, sep="",header=FALSE)
@@ -34,17 +27,3 @@ for (infile in list.files(folder)){
 
     #writing peak list in csv format
     write.table(d, file=path_out,append = FALSE,sep="\t", quote=FALSE,row.names=FALSE,col.names=c("Centroid Mass","S/N"))
-
-    #adding dummy values at 2000 and 20000 to comply to PPMC peak list standard
-    d <- rbind(d,"a"=c(2000,0))
-    d <- rbind(d,"b"=c(20000,0))
-    d <- d[order(d$V1),]
-    write.table(d, file=path_ppmc,append = FALSE,sep="\t", quote=FALSE,row.names=FALSE,col.names=c("Centroid Mass","S/N"))
-
-    #trimming m/z values below 3000 to output in the PKL4PPMC_TRIMMED directory
-    trim <- subset(d,V1>3000)
-    trim <- rbind(trim,"a"=c(2000,0))
-    trim <- rbind(trim,"b"=c(20000,0))
-    trim <- trim[order(trim$V1),]
-    write.table(trim, file=path_trim,append = FALSE,sep="\t", quote=FALSE,row.names=FALSE,col.names=c("Centroid Mass","S/N"))})
-    }
